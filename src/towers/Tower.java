3c64626c58cd7d7f2624cc60831cf6278e2d645d
Jonathan Woo <jonathan.ck.woo@gmail.com>
2019-05-16 13:09:49 -0400

    Update Tower.java

--- src/towers/Tower.java
+++ src/towers/Tower.java
@@ -1,10 +1,38 @@
 package towers;
 
-public class Tower {
+import java.awt.image.BufferedImage;
+
+import enemies.Enemy;
+import main.Utils;
+
+public abstract class Tower {
 	
 	//properties
+	String strName;
+	int intxLocation;
+	int intyLocation;
+	int intPrice;
+	int intRange;
+	int intAttackSpeed;
+	long longLastAttack;
+	BufferedImage towerImage;
 	
 	//methods
+	public boolean isInRange(Enemy enemy){
+		return true;
+	}
+	public void attack(Enemy enemy) {
+	}
 	
 	//constructor
+	public Tower(String strName, int intxLocation, int intyLocation, int intPrice, int intRange, int intAttackSpeed, String strTowerImage) {
+		this.strName = strName;
+		this.intxLocation = intxLocation;
+		this.intyLocation = intyLocation;
+		this.intPrice = intPrice;
+		this.intRange = intRange;
+		this.intAttackSpeed = intAttackSpeed;
+		this.towerImage = Utils.loadImage(strTowerImage);
+
+	}
 }
