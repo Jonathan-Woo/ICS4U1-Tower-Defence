20ab7add1827defe01e5ec0dd44fa67f1da292fb
Pablo <pablojose.araujocamacho19@ycdsbk12.ca>
2019-05-16 12:21:37 -0400

    Frame, Animation Panel, and State Creation
    Ignore the .classpath, .gitignore, and .project files.

---
+++ src/main/TowerDefence.java
@@ -0,0 +1,96 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import states.MainMenu;
+import states.State;
+
+//MAKE THIS MAIN CLASS THE JFRAME SO WE DON'T
+//NEED A SEPARATE OBJECT FOR THAT
+public class TowerDefence extends JFrame implements ActionListener {
+	
+	//KEEPS TRACK OF CURRENT GAME STATE
+	private State currentState;
+	private AnimationPanel pnl;
+	private Timer timer;
+	
+	public static void main(String[] args) {
+		new TowerDefence();
+	}
+	
+	public TowerDefence() {
+		//INIT JFRAME
+		super("Tower Defence");
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//SHOW FRAME IN THE MIDDLE OF THE SCREEN ON CREATION
+		this.setLocationRelativeTo(null);
+		
+		//INIT ANIMATION PANEL
+		pnl = new AnimationPanel(new Dimension(500, 500), this);
+		this.add(pnl);
+		
+		//INIT DEFAULT STATE OF THE GAME
+		currentState = new MainMenu();
+		
+		//SHOW FRAME
+		this.pack();
+		this.setVisible(true);
+		
+		//START TIMER
+		timer = new Timer(1000 / 60, this);
+		timer.start();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource().equals(timer)) {
+			//UPDATE STATE
+			currentState.update();
+			
+			//REPAINT ANIMATION PANEL
+			pnl.repaint();
+		}
+	}
+	
+	/**
+	 * Returns the current State of the game.
+	 * 
+	 * @return The current State object the game is using.
+	 */
+	public State getCurrentState() {
+		return currentState;
+	}
+	
+	//WE'LL MAKE THE ANIMATION PANEL CLASS IN HERE
+	//SINCE IT'S SO SMALL AND WE ONLY USE IT HERE
+	//WE DON'T NEED TO CREATE A WHOLE NEW FILE FOR IT
+	private class AnimationPanel extends JPanel {
+		
+		private TowerDefence towerDefence;
+		
+		public AnimationPanel(Dimension size, TowerDefence towerDefence) {
+			this.setPreferredSize(size);
+			this.towerDefence = towerDefence;
+		}
+		
+		@Override
+		public void paintComponent(Graphics g) {
+			//CLEAR PANEL
+			g.setColor(Color.BLACK);
+			g.fillRect(0, 0, this.getWidth(), this.getHeight());
+			
+			//RENDER BASED ON STATE
+			towerDefence.getCurrentState().render(g);
+		}
+		
+	}
+
+}
---
+++ src/states/State.java
@@ -0,0 +1,14 @@
+package states;
+
+import java.awt.Graphics;
+
+//BLUEPRINT FOR ALL STATE CLASSES
+public abstract class State {
+
+	//UPDATE METHOD THAT GETS CALLED EVERY GAME LOOP
+	public abstract void update();
+	
+	//RENDER METHOD TO DRAW GRAPHICS TO SCREEN
+	public abstract void render(Graphics g);
+	
+}
