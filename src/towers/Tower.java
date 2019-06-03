81f9d9788c099540ca3d482c0251b80b8f5c11d5
Pablo <pablojose.araujocamacho19@ycdsbk12.ca>
2019-06-03 13:36:52 -0400

    Update Tower.java

--- src/towers/Tower.java
+++ src/towers/Tower.java
@@ -16,8 +16,8 @@ import states.Game;
 public abstract class Tower {
 	
 	//properties
-	private static Map<String, String>[] towerFiles;
-	private static BufferedImage[] towerImages;
+	public static Map<String, String>[] towerFiles;
+	public static BufferedImage[] towerImages;
 	/**
 	 *Numerical representation of the basic tower.
 	 **/
@@ -92,11 +92,6 @@ public abstract class Tower {
 			Utils.loadImage("towers/" + towerFiles[Tower.FIRE].get("image")),
 			Utils.loadImage("towers/" + towerFiles[Tower.ICE].get("image"))
 		};
-	}
-	
-
-	public static BufferedImage getImage(int type) {
-		return towerImages[type];
 	}
 	
 	private void findEnemy(ArrayList<Enemy> enemies) {
